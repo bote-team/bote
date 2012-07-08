cf4eb594a20d2c3631e5f10520c74a2bf864e6dd
anonymissimus_cp <SND\anonymissimus_cp>
2012-07-08 15:54:58 +0200

    initialize CBotf2Doc::m_ptCurrentCombatSector

--- BotE Game/trunk/Source/Framework/botf2Doc.cpp
+++ BotE Game/trunk/Source/Framework/botf2Doc.cpp
@@ -59,6 +59,7 @@ END_MESSAGE_MAP()
 #pragma warning (disable:4351)
 CBotf2Doc::CBotf2Doc() :
 	m_ptKO(0, 0),
+	m_ptCurrentCombatSector(-1, -1),
 	m_NumberOfTheShipInArray(-1),
 	m_bCombatCalc(false),
 	m_bDataReceived(false),
