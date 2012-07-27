516ee5df80972b95c7d3c416346a2e294664b37a
Reginald_cp <SND\Reginald_cp>
2012-07-27 20:33:12 +0200

    removed old Alpha6 V0.80 from ..Game/tag/20110122_Alpha6_V0.80 (i have a local backup) (150 MB)

--- BotE Game/tag/20110122_Alpha6_V0.80/Source/StdAfx.cpp
+++
@@ -1,8 +0,0 @@
-// stdafx.cpp : Quelltextdatei, die nur die Standard-Includes einbindet
-//	botf2.pch ist die vorcompilierte Header-Datei
-//	stdafx.obj enthält die vorcompilierte Typinformation
-
-#include "stdafx.h"
-
-
-
