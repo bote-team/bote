196972b54e0a902166c2a3451d2470a55716749f
Reginald_cp <SND\Reginald_cp>
2012-07-28 18:58:47 +0200

    added Repo_DE_into_EN.bat to make easily an English BotE-Copy

---
+++ BotE Game/trunk/Repo_DE_into_EN.bat
@@ -0,0 +1,22 @@
+echo off
+echo *
+echo For next SVN-Update it is neccessary to let original files stored and to make a copy of BotE anywhere else
+echo *
+echo *** This will overwrite all files in c:\BotESVNCOPY_EN - ok? (or STOP with STRG+C) ***
+echo *
+pause
+
+xcopy game\*.* c:\BotESVNCOPY_EN\game\*.* /Y /S
+xcopy *.* c:\BotESVNCOPY_EN\*.* /Y 
+
+Echo *** now turns German into English ***
+xcopy c:\BotESVNCOPY_EN\game\Data\Buildings\[en]\*.* c:\BotESVNCOPY_EN\game\Data\Buildings\*.* /Y /S
+xcopy c:\BotESVNCOPY_EN\game\Data\Names\[en]\*.* c:\BotESVNCOPY_EN\game\Data\Names\*.* /Y /S
+xcopy c:\BotESVNCOPY_EN\game\Data\Other\[en]\*.* c:\BotESVNCOPY_EN\game\Data\Other\*.* /Y /S
+xcopy c:\BotESVNCOPY_EN\game\Data\Races\[en]\*.* c:\BotESVNCOPY_EN\game\Data\Races\*.* /Y /S
+xcopy c:\BotESVNCOPY_EN\game\Data\Ships\[en]\*.* c:\BotESVNCOPY_EN\game\Data\Ships\*.* /Y /S
+xcopy c:\BotESVNCOPY_EN\game\Data\Strings\[en]\*.* c:\BotESVNCOPY_EN\game\Data\Strings\*.* /Y /S
+echo on
+pause
+cd c:\BotESVNCOPY_EN
+explorer .
