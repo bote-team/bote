9b4c2ce3e1224e90ee57f9a2c51ef0f8a20d047f
Reginald_cp <SND\Reginald_cp>
2012-08-07 14:42:57 +0200

    - changed MajorPlanets.data (less more population) - added Batch-Files to switch (only in SVN-Copy!) between Original and the new extended (nr. 1)

---
+++ BotE Game/trunk/batch/All_To_Extended1.bat
@@ -0,0 +1,5 @@
+copy .\game\data\ships\StartShips_Extended1.data .\game\data\ships\StartShips.data
+
+copy .\game\data\Races\MajorPlanets_Extended1.data .\game\data\Races\MajorPlanets.data
+
+
---
+++ BotE Game/trunk/batch/All_To_Original.bat
@@ -0,0 +1,3 @@
+copy .\game\data\ships\StartShips.data.org .\game\data\ships\StartShips.data
+
+copy .\game\data\Races\MajorPlanets.data.org .\game\data\Races\MajorPlanets.data
