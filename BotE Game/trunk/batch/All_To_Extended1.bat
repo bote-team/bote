9cb1119c0aea92c21e5336029e5497bb37cd396f
Reginald_cp <SND\Reginald_cp>
2012-08-07 17:34:10 +0200

    - changed StartBuildings.data as described in German Forum - added Batch-Files to switch (only in SVN-Copy!) between original file  and the new extended (nr. 1)

--- BotE Game/trunk/batch/All_To_Extended1.bat
+++ BotE Game/trunk/batch/All_To_Extended1.bat
@@ -1,5 +1,7 @@
 copy .\game\data\ships\StartShips_Extended1.data .\game\data\ships\StartShips.data
 
+copy .\game\data\Buildings\StartBuildings_Extended1.data .\game\data\Buildings\StartBuildings.data
+
 copy .\game\data\Races\MajorPlanets_Extended1.data .\game\data\Races\MajorPlanets.data
 
 
--- BotE Game/trunk/batch/All_To_Original.bat
+++ BotE Game/trunk/batch/All_To_Original.bat
@@ -1,3 +1,5 @@
 copy .\game\data\ships\StartShips.data.org .\game\data\ships\StartShips.data
 
+copy .\game\data\Buildings\StartBuildings.data.org .\game\data\Buildings\StartBuildings.data
+
 copy .\game\data\Races\MajorPlanets.data.org .\game\data\Races\MajorPlanets.data
