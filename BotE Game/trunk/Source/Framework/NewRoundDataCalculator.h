468907ed25606d215bda404e6aedec330abc351a
anonymissimus_cp <SND\anonymissimus_cp>
2012-10-14 00:44:19 +0200

    remove extra qualification

--- BotE Game/trunk/Source/Framework/NewRoundDataCalculator.h
+++ BotE Game/trunk/Source/Framework/NewRoundDataCalculator.h
@@ -15,7 +15,7 @@ public:
 	void CheckRoutes(const CSector& sector, CSystem& system, CMajor* pMajor);
 	static void CalcIntelligenceBoni(const CSystemProd* production, CIntelligence* intelligence);
 	static void CalcMoral(const CSector& sector, CSystem& system, CArray<CTroopInfo>& TroopInfo);
-	static void CNewRoundDataCalculator::CalcExtraVisibilityAndRangeDueToDiplomacy(CSector& sector, const std::map<CString, CMajor*>* pmMajors);
+	static void CalcExtraVisibilityAndRangeDueToDiplomacy(CSector& sector, const std::map<CString, CMajor*>* pmMajors);
 	void AddShipPortsFromMinors(const std::map<CString, CMajor*>& pmMajors);
 	void CalcPreLoop();
 
