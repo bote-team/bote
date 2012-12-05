0c6b6c604d5292b210c6ce45b09244bb4c256d74
anonymissimus_cp <SND\anonymissimus_cp>
2012-12-05 17:28:09 +0100

    add a sanity check that all ships (in fleet or not) have an allowed order

--- BotE Game/trunk/Source/Framework/Sanity.cpp
+++ BotE Game/trunk/Source/Framework/Sanity.cpp
@@ -77,6 +77,15 @@ static void SanityCheckShip(const CShip& ship)
 			co.x, co.y);
 		Notify(s);
 	}
+	if(!ship.CanHaveOrder(ship.GetCurrentOrder())) {
+		CString s;
+		s.Format("The %s from %s at (%u, %u) has order %s which it should not be allowed to have!",
+			ship.GetCurrentOrderAsString(),
+			ship.GetShipName(),
+			ship.GetOwnerOfShip(),
+			co.x, co.y);
+		Notify(s);
+	}
 }
 
 void CSanity::SanityCheckFleet(const CShips& ship)
