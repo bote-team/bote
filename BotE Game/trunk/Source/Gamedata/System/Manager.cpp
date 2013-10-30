bote-team/bote

commit 5242bbe2670404f9a7564621a281b318b1a947b7

fix an infinte loop

BotE Game/trunk/Source/Gamedata/System/Manager.cpp
@@ -383,6 +383,8 @@ private:
 			SetWorker(WORKER::INDUSTRY_WORKER, CSystem::SET_WORKER_MODE_DECREMENT);
 			++unset;
 			m_pSystem->CalculateVariables();
+			if(m_pSystem->GetWorker(WORKER::INDUSTRY_WORKER) == 0)
+				return unset;
 			if(min_rounds < m_pSystem->NeededRoundsToBuild(0, true))
 			{
 				SetWorker(WORKER::INDUSTRY_WORKER, CSystem::SET_WORKER_MODE_INCREMENT);

commit 219c471416a5bb7e64ba7d2ca317ed31b1be33ca

fix something in the "find ideal number of industry workers" code
5242bbe2670404f9a7564621a281b318b1a947b7 introduced another problem;
if the functions exits due to workers being at 0, removing the last
worker may (probably has) increased the turns needed to build,
so undo this worker removal in this case.

BotE Game/trunk/Source/Gamedata/System/Manager.cpp
@@ -365,6 +365,13 @@ private:
 		return unset;
 	}
 
+	void OnBestIndustryWorkerCountFound(int& unset)
+	{
+		SetWorker(WORKER::INDUSTRY_WORKER, CSystem::SET_WORKER_MODE_INCREMENT);
+		--unset;
+		assert(unset >= 0);
+	}
+
 	//removes workers from factories until they are the minimum number to finish the current project
 	//in still the same number of turns (as at the start of the function)
 	int DecrementDueToWastedIndustry(bool max_industry)
@@ -383,13 +390,16 @@ private:
 			SetWorker(WORKER::INDUSTRY_WORKER, CSystem::SET_WORKER_MODE_DECREMENT);
 			++unset;
 			m_pSystem->CalculateVariables();
+			const int current_rounds = m_pSystem->NeededRoundsToBuild(0, true, false);
 			if(m_pSystem->GetWorker(WORKER::INDUSTRY_WORKER) == 0)
-				return unset;
-			if(min_rounds < m_pSystem->NeededRoundsToBuild(0, true))
 			{
-				SetWorker(WORKER::INDUSTRY_WORKER, CSystem::SET_WORKER_MODE_INCREMENT);
-				--unset;
-				assert(unset >= 0);
+				if(min_rounds < current_rounds)
+					OnBestIndustryWorkerCountFound(unset);
+				return unset;
+			}
+			if(min_rounds < current_rounds)
+			{
+				OnBestIndustryWorkerCountFound(unset);
 				return unset;
 			}
 		}